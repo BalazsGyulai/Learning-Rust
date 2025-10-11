b5778f15bf874ca664bdae972b70eaa9fc5e504f
Balazs Gyulai <gyulai.balazs2005@gmail.com>
2025-10-11 18:07:19 +0200

    GTA can give back the correct sum result but not the individual ones

--- session 3/heist.rs
+++ session 3/heist.rs
@@ -2,48 +2,63 @@ fn main() {
     // plans the ultimate heist in preparation for GTA VI, picking the best
     // combination of items to put into a bag of capacity k, where each item has
     // some value v and some weight w
+
     let loot: Vec<(&str, f32, f32)> = [
-        ("Art", 300.0, 11.0),
-        ("Stamp", 300.0, 0.3),
-        ("Diamond", 100.0, 0.6),
-        ("Gold", 200.0, 10.0),
-        ("Money", 200.0, 1.0),
+        ("Art", 3.0, 2.0),
+        ("Stamp", 4.0, 3.0),
+        ("Diamond", 5.0, 4.0),
+        ("Gold", 8.0, 5.0),
+        ("Money", 10.0, 9.0),
     ].into();
-    
-    let mut memo: Vec<f32> = Vec::new();
-    init_array(loot.len() as i64, &mut memo);
-    let mut capacity: f32 = 5.0;
-    dp_top_down((loot.len() as i64) - 1, &mut memo, &loot, &mut capacity);
-    println!("Final: {:?}", memo);
+    let loot: Vec<(&str, f32, f32)> = [
+        ("Art", 60.0, 10.0),
+        ("Stamp", 100.0, 20.0),
+        ("Diamond", 120.0, 30.0),
+
+    ].into();
+
+    let capacity: f32 = 50.0;
+    // dp_top_down((loot.len() as i64) - 1, &mut memo, &loot, &mut capacity);
+    // bottom_up(capacity, &mut memo, &loot);
+    // println!("Final: {:?}", memo);
+    println!("{:?}", top_down(0, capacity, &loot));
 }
 
-fn dp_top_down(i: i64, memo: &mut Vec<f32>, loot: &Vec<(&str, f32, f32)>, capacity: &mut f32) -> f32{
-    println!("Got {}, Memo {:?}", i, memo);
+fn top_down(
+    i: u32,
+    capacity: f32,
+    loot: &Vec<(&str, f32, f32)>
+) -> (f32, f32) {
+    if i == (loot.len() as u32) || capacity <= 0.0 {
+        return (0.0, 0.0);
+    }
 
-    if i == 1{
-        if loot[0].2 <= capacity{
-            memo[0] = loot[0].2;
-            return memo[0];
+    let (value_i, weight_i) = (loot[i as usize].1, loot[i as usize].2);
+
+    let excluded = top_down(i + 1, capacity, loot);
+
+    let included = {
+        if can_we_include(loot[i as usize], capacity){
+            let (sub_v, sub_w) = top_down(i + 1, capacity - weight_i, loot);
+            (value_i + sub_v, weight_i + sub_w)
+        } else {
+            (0.0, 0.0)
         }
-    }
+    };
 
-    if memo[i] != -1{
-        return memo[i]
-    }
-
+    return choose_max(included, excluded);
 }
 
-fn choose_max(a: f32, b: f32) -> f32{
-    if a >= b{
-        a
-    } else {
-        b
-    }
+fn can_we_include(item: (&str, f32, f32), capacity: f32) -> bool{
+    item.2 <= capacity
 }
 
-fn init_array(n: i64, array: &mut Vec<i64>){
-    for _ in 0..n{
-        array.push(-1);
-    }
+fn choose_max(a: (f32, f32), b: (f32, f32)) -> (f32, f32) {
+    if a.0 >= b.0 { a } else { b }
 }
 
+fn init_array(n: i64, array: &mut Vec<(f32, f32)>) {
+    for _ in 0..=n + 1 {
+        array.push((-1.0, -1.0));
+    }
+}
